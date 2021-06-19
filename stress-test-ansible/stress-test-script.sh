4d8e857db031a766e27dec31e7364a1aa90842bd
sodbaveka <sodbaveka@gmail.com>
2021-06-19 12:29:12 +0200

    Refactoring strses-test files

--- stress-test-ansible/stress-test-script.sh
+++ stress-test-ansible/stress-test-script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "$1 ec1" >> /etc/hosts
 echo "$2 ec2" >> /etc/hosts
-ansible-playbook -i stress-test-inventory.ini stress-test-playbook.yaml 
+ansible-playbook -i stress-test-inventory.yml stress-test-playbook.yml 
